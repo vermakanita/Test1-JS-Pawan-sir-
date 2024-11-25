16969e8e7a1996429b092eae043e3321ccba6d56
KANITA VERMA <vermakanita13@gmail.com>
2024-11-25 18:54:49 +0530

    Add files via upload

---
+++ _19ArrowAndRegularFunction.js
@@ -0,0 +1,25 @@
+/*
+regular : have own this
+used as constructor
+with name
+
+arroe: take this to globle 
+cant use constor
+without name
+*/
+
+// regular 
+// function f1(){
+//     console.log("regular")
+// }
+// f1()
+
+// let arrow= ()=>{
+//     console.log("arrow")
+// }
+// ()
+
+
+ 
+  
+    
