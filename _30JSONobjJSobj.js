25811c2d7d46256ea287f9d874005add082bd454
KANITA VERMA <vermakanita13@gmail.com>
2024-11-27 20:31:55 +0530

    Add files via upload

---
+++ _30JSONobjJSobj.js
@@ -0,0 +1,13 @@
+/*
+jsobj:- 
+1.native object flexible
+2.support method function 
+3.not compulsary to give the key in double code 
+4.single code key accepteble
+*/
+
+let json='{"name:"k","age":23}';
+let obj=JSON.parse(json);//JSON
+
+
+let jsOBJ={name:"k",age:23}
