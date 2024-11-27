1797235e1bcc6f6ae7c7ccf833d38093abe9c45b
KANITA VERMA <vermakanita13@gmail.com>
2024-11-27 20:32:14 +0530

    Add files via upload

---
+++ _31varLet.js
@@ -0,0 +1,20 @@
+/*
+var:-
+redeclared
+globle scope
+hoisting but initialized as undefined 
+
+
+let :
+blocked scope 
+can't redeclare 
+hoisting but not initilzed 
+*/
+
+var x=10;
+var x=10;
+
+let y=10;
+// let y=10;
+
+
