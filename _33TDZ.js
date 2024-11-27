8d7d57c252499a888a30f19553a310c9c7457320
KANITA VERMA <vermakanita13@gmail.com>
2024-11-27 20:33:02 +0530

    Add files via upload

---
+++ _33TDZ.js
@@ -0,0 +1,7 @@
+/*
+
+temporal dead zone:- it is the time between the start a block scope and when the veriable declared with let and const at initialized.accesing the veriable during the tdz it throws a referenceerror.
+*/
+
+console.log(x);
+let x=10;
